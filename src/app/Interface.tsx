4aba7bc39aa5aed29435350c7e1952e8e274b2d1
MrOmarImtiaz <168307606+MrOmarImtiaz@users.noreply.github.com>
2024-09-24 21:44:56 +0500

    Updated

---
+++ src/app/Interface.tsx
@@ -0,0 +1,176 @@
+import { useState} from "react";
+
+
+const Interface = () => {
+  const [input1, setInput1] = useState<string>('');
+  const [output1, setOutput1] = useState<string>('');
+  const [output2, setOutput2] = useState<string>('');
+  const [isLoading1, setIsLoading1] = useState<boolean>(false);
+  const [isLoading2, setIsLoading2] = useState<boolean>(false);
+
+  //Langbase API call for Prospector Pipe
+  const handleSubmit1 = async (e: React.FormEvent) => {
+    e.preventDefault();
+	  setIsLoading1(true);
+    try {
+      const response = await fetch('https://api.langbase.com/beta/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.NEXT_PUBLIC_API_KEY1}`,
+        },
+        body: JSON.stringify({ variables: [
+			{name: 'content', value: input1} 
+	]}),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data1 = await response.json();
+	  const responseData1 = data1.completion || 'No description available.';
+      setOutput1(responseData1); 
+    } catch (error) {
+      console.error('Error:', error);
+    }finally {
+		setIsLoading1(false); 
+	  }
+  };
+
+  //Langbase API call for Emails Pipe
+  const handleSubmit2 = async (e: React.FormEvent) => {
+    e.preventDefault();
+	  setIsLoading2(true);
+    try {
+      const response = await fetch('https://api.langbase.com/beta/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.NEXT_PUBLIC_API_KEY2}`,
+        },
+        body: JSON.stringify({ variables: [
+			{name: 'list', value: output1} 
+	]}),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data2 = await response.json();
+	  const responseData2 = data2.completion || 'No description available.';
+      setOutput2(responseData2); 
+    } catch (error) {
+      console.error('Error:', error);
+    }finally {
+		setIsLoading2(false); 
+	  }
+  };  
+  
+  
+  
+  
+  return (
+    
+    //Grid of 3 columns
+    <div className="grid grid-cols-3 h-screen w-screen bg-muted">
+      
+      {/*Column 1: Card for input */}
+      <div className="flex justify-center items-center">
+        <div className="bg-background  p-6 rounded-lg shadow-lg border-2 border-border" style={{ width: "375px", height: "700px" }}>
+          
+          {/* Input label */}
+            <div className="flex justify-center">
+              <button className="font-mono px-4 py-2 mb-4 bg-black  text-foreground font-semibold rounded-md">
+                INPUT
+              </button>
+            </div>
+            
+            {/* Description below Button */}
+            <p className="text-muted-foreground text-center mb-4 font-sans">
+              Paste your content from which you want to prospect in the space below.
+            </p>
+
+            {/* Output Text Area */}
+            <textarea
+              value={input1}
+              onChange={(e) => {
+              setInput1(e.target.value);
+              console.log("Input:", e.target.value); // Log input value
+              }}
+              className="placeholder:text-center placeholder:italic placeholder:text-muted-foreground font-sans w-full p-2 border-2 border-border rounded-md  bg-muted text-foreground overflow-auto focus:outline-none"
+              rows = {21}
+              placeholder="Paste content here..."
+          
+            />
+
+        </div>
+      </div>
+
+      {/*Column 2: Card for input */}
+      <div className="flex justify-center items-center">
+        <div className="bg-background  p-6 rounded-lg shadow-lg border-2 border-border" style={{ width: "375px", height: "700px" }}>
+          
+          {/* Button for Prospecting */}
+          <div className="flex justify-center">
+            <button 
+              type = "button"
+              onClick={handleSubmit1}
+              className="font-mono px-4 py-2 mb-4 bg-black  text-white font-semibold rounded-md">
+              PROSPECT
+              </button>
+          </div>
+        
+        {/* Description below Button */}
+          <p className="text-muted-foreground text-center mb-4 font-sans">
+            Press the button above to generate a list of potential customer companies.
+          </p>
+
+        {/* Prospecting List Output */}
+          <textarea
+            value = {isLoading1 ? 'AI is Processing...' : output1}
+            className= "placeholder:text-center placeholder:italic placeholder:text-muted-foreground font-sans w-full p-2 border-2 border-border rounded-md  bg-muted text-foreground overflow-auto focus:outline-none"
+            rows = {21}
+            placeholder="Press Prospect to generate output..."
+            readOnly
+          />
+
+        </div>
+      </div>
+      
+      {/*Column 1: Card for input */}
+      <div className="flex justify-center items-center">
+        <div className="bg-background  p-6 rounded-lg shadow-lg border-2 border-border" style={{ width: "375px", height: "700px" }}>
+          
+          {/* Button for Emails */}
+          <div className="flex justify-center">
+            <button
+              type = "button"
+              onClick={handleSubmit2}
+              className="font-mono px-6 py-2 mb-4 bg-black  text-white font-semibold rounded-md">
+              EMAILS 
+            </button>
+          </div>
+          
+          {/* Description below Button */}
+          <p className="text-muted-foreground text-center mb-4 font-sans">
+            Press the button above to generate sign-up emails for the prospected comppanies.
+          </p>
+
+          {/* Emails Output*/}
+          <textarea
+            value = {isLoading2 ? 'AI is Processing...' : output2}
+            className=" placeholder:text-center placeholder:italic placeholder:text-muted-foreground font-sans w-full p-2 border-2 border-border rounded-md  bg-muted text-foreground overflow-auto focus:outline-none"
+            rows = {21}
+            placeholder="Press Emails to generate output..."
+            readOnly
+          />
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Interface;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,53 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+// components/IntroScreen.tsx
+import { useState} from "react";
+import Interface from "./Interface"; // Adjust the path if necessary
+
+const IntroScreen = () => {
+  const [showCard, setShowCard] = useState(false);
+
+
+  const handleClick = () => {
+    setShowCard(true)};
+ 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="max-h-screen max-w-screen overflow-hidden">
+      <div className="absolute inset-0 flex flex-col items-center justify-center bg-background">
+        {!showCard && (
+        <h1 className="absolute font-mono top-12 text-6xl md:text-6xl tracking-wide font-bold  text-center text-transparent bg-clip-text bg-foreground w-full">
+          PROSPECTOR
+        </h1>
+        )}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+        {!showCard && (
+          <>
+            {/* Responsive Logo */}
+            <img
+              src="/langbase.png"
+              alt="Logo"
+              className="w-[200px] md:w-[300px] h-auto mt-[150px] md:mt-[210px] transition-transform duration-300 transform hover:scale-110"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            
+            {/* Responsive Button */}
+            <button
+              onClick={handleClick}
+              className="font-mono mt-[100px] md:mt-[155px] px-4 md:px-6 py-2 md:py-3 bg-black text-white text-lg md:text-xl font-bold rounded-lg shadow-lg  transition-transform duration-300 transform hover:scale-110"
+            >
+              GO TO APP
+            </button>
+          </>
+        )}
+
+          {/* Show the card when the button is clicked */}
+        
+          {showCard &&(
+             <Interface />
+           )}
+         
+      </div>
     </div>
   );
-}
+};
+
+export default IntroScreen;
